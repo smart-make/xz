bf388898dd0591e94e25bedfd2c6f9392241a6a8
Duzy Chan <code@duzy.info>
2013-03-24 20:30:04 +0800

    update smart scripts

---
+++ src/liblzma/common/config.h
@@ -0,0 +1,8 @@
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_STDBOOL_H 1
+#define HAVE_VISIBILITY 1
+#define HAVE_STRING_H 1
+/* #undef HAVE_PTHREAD */
+#define restrict
